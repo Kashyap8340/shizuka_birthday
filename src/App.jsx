57f13c8016d60a21fcaa61c346650eca99797bc4
aditya anand <2020adityanand@gmail.com>
2026-02-15 22:31:32 +0530

    Update countdown page with personalized message

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ function App() {
               </>
             )}
           </h1>
-          <p>Your personalized message goes here 💗</p>
+          <p>Love you bebbo just wait... 💗</p>
         </section>
 
         <Countdown
